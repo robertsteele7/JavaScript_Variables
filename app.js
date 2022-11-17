3b35510abf4e898df407bc359815d32e079c197f
robertsteele7 <atpcycle7@gmail.com>
2022-11-17 16:11:10 -0600

    exercise complete

--- app.js
+++ app.js
@@ -5,6 +5,38 @@ console.log(
 
 // Exercise 1
 
-// Exercise 2
+const firstName = `Rob`;
+let lastName = `Steele`;
+var myAge = 53;
 
+// Exercise 2 
+let fullName = firstName + " " + lastName + " " + myAge;
+console.log("fullName:" , fullName);
+console.log("=====================================");
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+myProgram = ` The language is ${language} created in ${createdYear} and is case sensitive: ${isCaseSensitive}!`;
+console.log(myProgram);
+console.log("=====================================");
+//Exercise 4: Declaring and Assigning Values to Variables x2
+let price = 19.99;
+let isOnSale = false;
+let SalePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+myNewShirt = `My new shirt cost ${price} is showing going on sale to be ${isOnSale} will be going ${SalePercentage}% off with my coupon but currently they show stock of ${stock} and showing in stock as ${inStock} and im looking for a size ${selectedSize} hope they get one in stock before the coupon expires`;
+
+console.log(myNewShirt);
+console.log("=====================================");
+//Exercise 5: Declaring and Assigning Values to Variables x3
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = `true`;
+myBook = `The name of the Book was ${title} by an author named ${author}. It was a long ${pageCount} pages and I'm currently at page ${bookMark} but im checking it off as read: ${hasRead} because i will have it finished by saturday!`;
+console.log(myBook);
+console.log("=====================================");
